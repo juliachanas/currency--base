0824b02693efa734224099908b70dc0d76f9fa0f
Julia Chanas <juulia.chanas@gmail.com>
2024-01-29 16:03:00 +0100

    Add test for conversion  USD -> PLN, and PLN -> PLN and USD -> USD

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -6,7 +6,7 @@ describe('Component ResultBox', () => {
   it('should render without crashing', () => {
     render(<ResultBox from='PLN' to='USD' amount={100} />);
   });
-  it('should render proper info about conversion when PLN -> USD', () => {
+  it('should render proper info about conversion PLN -> USD', () => {
     const testCases = [
       {
         from: 'PLN',
@@ -31,6 +31,81 @@ describe('Component ResultBox', () => {
         to: 'USD',
         amount: 1000,
         expectedOutput: 'PLN 1,000.00 = $285.71',
+      },
+    ];
+
+    for (const testObj of testCases) {
+      const { from, to, amount, expectedOutput } = testObj;
+
+      // render component and store the result
+      render(<ResultBox from={from} to={to} amount={amount} />);
+
+      // find the output element
+      const output = screen.getByTestId('output');
+      // assert the text content
+      expect(output).toHaveTextContent(expectedOutput);
+
+      // unmount component
+      cleanup();
+    }
+  });
+  it('should render proper info about conversion when USD -> PLN', () => {
+    const testCases = [
+      {
+        from: 'USD',
+        to: 'PLN',
+        amount: 20,
+        expectedOutput: '$20.00 = PLN 70.00',
+      },
+      {
+        from: 'USD',
+        to: 'PLN',
+        amount: 345,
+        expectedOutput: '$345.00 = PLN 1,207.50',
+      },
+      {
+        from: 'USD',
+        to: 'PLN',
+        amount: 1200,
+        expectedOutput: '$1,200.00 = PLN 4,200.00',
+      },
+      {
+        from: 'USD',
+        to: 'PLN',
+        amount: 1,
+        expectedOutput: '$1.00 = PLN 3.50',
+      },
+    ];
+
+    for (const testObj of testCases) {
+      const { from, to, amount, expectedOutput } = testObj;
+
+      // render component and store the result
+      render(<ResultBox from={from} to={to} amount={amount} />);
+
+      // find the output element
+      const output = screen.getByTestId('output');
+      // assert the text content
+      expect(output).toHaveTextContent(expectedOutput);
+
+      // unmount component
+      cleanup();
+    }
+  });
+
+  it('should render proper info about when from = to', () => {
+    const testCases = [
+      {
+        from: 'USD',
+        to: 'USD',
+        amount: 20,
+        expectedOutput: '$20.00 = $20.00',
+      },
+      {
+        from: 'PLN',
+        to: 'PLN',
+        amount: 345,
+        expectedOutput: 'PLN 345.00 = PLN 345.00',
       },
     ];
 
